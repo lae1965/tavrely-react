494936b3f0a6ebf955c3ca5b131ec8de5c8453da
Andrey <lae1965@yandex.ru>
2022-04-01 16:47:21 +0300

    Work version 1.7

---
+++ src/Constants/games.js
@@ -0,0 +1,360 @@
+export const championship = [
+    {
+        name: 'Чемпионат России среди мужчин', 
+        town: 'Смоленск', 
+        year: 2013,
+        id: 'game-1' 
+    },
+    {
+        name: 'Чемпионат Европы среди мужчин', 
+        town: 'Москва', 
+        year: 2012,
+        id: 'game-2' 
+    },
+    {
+        name: 'Чемпионат Европы среди женщин', 
+        town: 'Санкт-Петербург', 
+        year: 2011,
+        id: 'game-3' 
+    },
+    {
+        name: 'Чемпионат мира среди женщин', 
+        town: 'Пермь', 
+        year: 2010,
+        id: 'game-4' 
+    },
+    {
+        name: 'Чемпионат мира среди мужчин', 
+        town: 'Нижний Новгород', 
+        year: 2009,
+        id: 'game-5' 
+    },
+    {
+        name: 'Чемпионат России среди женщин', 
+        town: 'Тюмень', 
+        year: 2008,
+        id: 'game-6' 
+    }
+];
+
+export const games = {
+    'game-1': {
+        whiteGamer: 'Антропов',
+        blackGamer: 'Журавлев',
+        score: '1-0',
+        moves: [
+            { white: 'c1 - d2', black: 'f8 - e9' },
+            { white: 'd1 - d2', black: 'c8 - d7' },
+            { white: 'b1 - c3', black: 'd8 - e7' },
+            { white: 'f1 - g2', black: 'b8 - c6' },
+            { white: 'g1 - e2', black: 'e7 - d7' },
+            { white: 'd2 - d5', black: '0 - 0 - 0' },
+            { white: '0 - 0 - 0', black: 'g8 - f6' },
+            { white: 'e2(1) - c3', black: 'd7(4) - g4', comment: 'Преждевременно и проигрывает, лучше 8. … h8-d8.' },
+            { white: 'd5(1) - c6', black: 'g4 - g6' },
+            { white: 'c6 - b7', black: 'c8 - d8' },
+            { white: 'g2 - e4', black: 'g6 - g5+' },
+            { white: 'c1 - b1', black: 'g5 - e5' },
+            { white: 'c3 - a4', black: 'h8 - e8' },
+            { white: 'f2 - f3', black: 'e8 - e6' },
+            { white: 'b7 - d5', black: 'd8(1) - e8' },
+            { white: 'h1 - d1', black: 'e5 - h2', 'Позиция черных проиграна.' },
+            { white: 'a4 - c5', black: 'h2 - e2' },
+            { white: 'd5 - d7', black: 'f6 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1 - d7' }
+        ],
+        result: 'Черные сдались.'
+    }
+    'game-1': {
+        whiteGamer: 'Антропов',
+        blackGamer: 'Журавлев',
+        score: '1-0',
+        moves: [
+            { white: 'c1 - d2', black: 'f8 - e9' },
+            { white: 'd1 - d2', black: 'c8 - d7' },
+            { white: 'b1 - c3', black: 'd8 - e7' },
+            { white: 'f1 - g2', black: 'b8 - c6' },
+            { white: 'g1 - e2', black: 'e7 - d7' },
+            { white: 'd2 - d5', black: '0 - 0 - 0' },
+            { white: '0 - 0 - 0', black: 'g8 - f6' },
+            { white: 'e2(1) - c3', black: 'd7(4) - g4', comment: 'Преждевременно и проигрывает, лучше 8. … h8-d8.' },
+            { white: 'd5(1) - c6', black: 'g4 - g6' },
+            { white: 'c6 - b7', black: 'c8 - d8' },
+            { white: 'g2 - e4', black: 'g6 - g5+' },
+            { white: 'c1 - b1', black: 'g5 - e5' },
+            { white: 'c3 - a4', black: 'h8 - e8' },
+            { white: 'f2 - f3', black: 'e8 - e6' },
+            { white: 'b7 - d5', black: 'd8(1) - e8' },
+            { white: 'h1 - d1', black: 'e5 - h2', 'Позиция черных проиграна.' },
+            { white: 'a4 - c5', black: 'h2 - e2' },
+            { white: 'd5 - d7', black: 'f6 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1 - d7' }
+        ],
+        result: 'Черные сдались.'
+    }
+    'game-1': {
+        whiteGamer: 'Антропов',
+        blackGamer: 'Журавлев',
+        score: '1-0',
+        moves: [
+            { white: 'c1 - d2', black: 'f8 - e9' },
+            { white: 'd1 - d2', black: 'c8 - d7' },
+            { white: 'b1 - c3', black: 'd8 - e7' },
+            { white: 'f1 - g2', black: 'b8 - c6' },
+            { white: 'g1 - e2', black: 'e7 - d7' },
+            { white: 'd2 - d5', black: '0 - 0 - 0' },
+            { white: '0 - 0 - 0', black: 'g8 - f6' },
+            { white: 'e2(1) - c3', black: 'd7(4) - g4', comment: 'Преждевременно и проигрывает, лучше 8. … h8-d8.' },
+            { white: 'd5(1) - c6', black: 'g4 - g6' },
+            { white: 'c6 - b7', black: 'c8 - d8' },
+            { white: 'g2 - e4', black: 'g6 - g5+' },
+            { white: 'c1 - b1', black: 'g5 - e5' },
+            { white: 'c3 - a4', black: 'h8 - e8' },
+            { white: 'f2 - f3', black: 'e8 - e6' },
+            { white: 'b7 - d5', black: 'd8(1) - e8' },
+            { white: 'h1 - d1', black: 'e5 - h2', 'Позиция черных проиграна.' },
+            { white: 'a4 - c5', black: 'h2 - e2' },
+            { white: 'd5 - d7', black: 'f6 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1 - d7' }
+        ],
+        result: 'Черные сдались.'
+    }
+    'game-1': {
+        whiteGamer: 'Антропов',
+        blackGamer: 'Журавлев',
+        score: '1-0',
+        moves: [
+            { white: 'c1 - d2', black: 'f8 - e9' },
+            { white: 'd1 - d2', black: 'c8 - d7' },
+            { white: 'b1 - c3', black: 'd8 - e7' },
+            { white: 'f1 - g2', black: 'b8 - c6' },
+            { white: 'g1 - e2', black: 'e7 - d7' },
+            { white: 'd2 - d5', black: '0 - 0 - 0' },
+            { white: '0 - 0 - 0', black: 'g8 - f6' },
+            { white: 'e2(1) - c3', black: 'd7(4) - g4', comment: 'Преждевременно и проигрывает, лучше 8. … h8-d8.' },
+            { white: 'd5(1) - c6', black: 'g4 - g6' },
+            { white: 'c6 - b7', black: 'c8 - d8' },
+            { white: 'g2 - e4', black: 'g6 - g5+' },
+            { white: 'c1 - b1', black: 'g5 - e5' },
+            { white: 'c3 - a4', black: 'h8 - e8' },
+            { white: 'f2 - f3', black: 'e8 - e6' },
+            { white: 'b7 - d5', black: 'd8(1) - e8' },
+            { white: 'h1 - d1', black: 'e5 - h2', 'Позиция черных проиграна.' },
+            { white: 'a4 - c5', black: 'h2 - e2' },
+            { white: 'd5 - d7', black: 'f6 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1 - d7' }
+        ],
+        result: 'Черные сдались.'
+    }
+    'game-1': {
+        whiteGamer: 'Антропов',
+        blackGamer: 'Журавлев',
+        score: '1-0',
+        moves: [
+            { white: 'c1 - d2', black: 'f8 - e9' },
+            { white: 'd1 - d2', black: 'c8 - d7' },
+            { white: 'b1 - c3', black: 'd8 - e7' },
+            { white: 'f1 - g2', black: 'b8 - c6' },
+            { white: 'g1 - e2', black: 'e7 - d7' },
+            { white: 'd2 - d5', black: '0 - 0 - 0' },
+            { white: '0 - 0 - 0', black: 'g8 - f6' },
+            { white: 'e2(1) - c3', black: 'd7(4) - g4', comment: 'Преждевременно и проигрывает, лучше 8. … h8-d8.' },
+            { white: 'd5(1) - c6', black: 'g4 - g6' },
+            { white: 'c6 - b7', black: 'c8 - d8' },
+            { white: 'g2 - e4', black: 'g6 - g5+' },
+            { white: 'c1 - b1', black: 'g5 - e5' },
+            { white: 'c3 - a4', black: 'h8 - e8' },
+            { white: 'f2 - f3', black: 'e8 - e6' },
+            { white: 'b7 - d5', black: 'd8(1) - e8' },
+            { white: 'h1 - d1', black: 'e5 - h2', 'Позиция черных проиграна.' },
+            { white: 'a4 - c5', black: 'h2 - e2' },
+            { white: 'd5 - d7', black: 'f6 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1 - d7' }
+        ],
+        result: 'Черные сдались.'
+    }
+    'game-1': {
+        whiteGamer: 'Антропов',
+        blackGamer: 'Журавлев',
+        score: '1-0',
+        moves: [
+            { white: 'c1 - d2', black: 'f8 - e9' },
+            { white: 'd1 - d2', black: 'c8 - d7' },
+            { white: 'b1 - c3', black: 'd8 - e7' },
+            { white: 'f1 - g2', black: 'b8 - c6' },
+            { white: 'g1 - e2', black: 'e7 - d7' },
+            { white: 'd2 - d5', black: '0 - 0 - 0' },
+            { white: '0 - 0 - 0', black: 'g8 - f6' },
+            { white: 'e2(1) - c3', black: 'd7(4) - g4', comment: 'Преждевременно и проигрывает, лучше 8. … h8-d8.' },
+            { white: 'd5(1) - c6', black: 'g4 - g6' },
+            { white: 'c6 - b7', black: 'c8 - d8' },
+            { white: 'g2 - e4', black: 'g6 - g5+' },
+            { white: 'c1 - b1', black: 'g5 - e5' },
+            { white: 'c3 - a4', black: 'h8 - e8' },
+            { white: 'f2 - f3', black: 'e8 - e6' },
+            { white: 'b7 - d5', black: 'd8(1) - e8' },
+            { white: 'h1 - d1', black: 'e5 - h2', 'Позиция черных проиграна.' },
+            { white: 'a4 - c5', black: 'h2 - e2' },
+            { white: 'd5 - d7', black: 'f6 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1 - d7' }
+        ],
+        result: 'Черные сдались.'
+    }
+    'game-1': {
+        whiteGamer: 'Антропов',
+        blackGamer: 'Журавлев',
+        score: '1-0',
+        moves: [
+            { white: 'c1 - d2', black: 'f8 - e9' },
+            { white: 'd1 - d2', black: 'c8 - d7' },
+            { white: 'b1 - c3', black: 'd8 - e7' },
+            { white: 'f1 - g2', black: 'b8 - c6' },
+            { white: 'g1 - e2', black: 'e7 - d7' },
+            { white: 'd2 - d5', black: '0 - 0 - 0' },
+            { white: '0 - 0 - 0', black: 'g8 - f6' },
+            { white: 'e2(1) - c3', black: 'd7(4) - g4', comment: 'Преждевременно и проигрывает, лучше 8. … h8-d8.' },
+            { white: 'd5(1) - c6', black: 'g4 - g6' },
+            { white: 'c6 - b7', black: 'c8 - d8' },
+            { white: 'g2 - e4', black: 'g6 - g5+' },
+            { white: 'c1 - b1', black: 'g5 - e5' },
+            { white: 'c3 - a4', black: 'h8 - e8' },
+            { white: 'f2 - f3', black: 'e8 - e6' },
+            { white: 'b7 - d5', black: 'd8(1) - e8' },
+            { white: 'h1 - d1', black: 'e5 - h2', 'Позиция черных проиграна.' },
+            { white: 'a4 - c5', black: 'h2 - e2' },
+            { white: 'd5 - d7', black: 'f6 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1 - d7' }
+        ],
+        result: 'Черные сдались.'
+    }
+    'game-1': {
+        whiteGamer: 'Антропов',
+        blackGamer: 'Журавлев',
+        score: '1-0',
+        moves: [
+            { white: 'c1 - d2', black: 'f8 - e9' },
+            { white: 'd1 - d2', black: 'c8 - d7' },
+            { white: 'b1 - c3', black: 'd8 - e7' },
+            { white: 'f1 - g2', black: 'b8 - c6' },
+            { white: 'g1 - e2', black: 'e7 - d7' },
+            { white: 'd2 - d5', black: '0 - 0 - 0' },
+            { white: '0 - 0 - 0', black: 'g8 - f6' },
+            { white: 'e2(1) - c3', black: 'd7(4) - g4', comment: 'Преждевременно и проигрывает, лучше 8. … h8-d8.' },
+            { white: 'd5(1) - c6', black: 'g4 - g6' },
+            { white: 'c6 - b7', black: 'c8 - d8' },
+            { white: 'g2 - e4', black: 'g6 - g5+' },
+            { white: 'c1 - b1', black: 'g5 - e5' },
+            { white: 'c3 - a4', black: 'h8 - e8' },
+            { white: 'f2 - f3', black: 'e8 - e6' },
+            { white: 'b7 - d5', black: 'd8(1) - e8' },
+            { white: 'h1 - d1', black: 'e5 - h2', 'Позиция черных проиграна.' },
+            { white: 'a4 - c5', black: 'h2 - e2' },
+            { white: 'd5 - d7', black: 'f6 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1 - d7' }
+        ],
+        result: 'Черные сдались.'
+    }
+    'game-1': {
+        whiteGamer: 'Антропов',
+        blackGamer: 'Журавлев',
+        score: '1-0',
+        moves: [
+            { white: 'c1 - d2', black: 'f8 - e9' },
+            { white: 'd1 - d2', black: 'c8 - d7' },
+            { white: 'b1 - c3', black: 'd8 - e7' },
+            { white: 'f1 - g2', black: 'b8 - c6' },
+            { white: 'g1 - e2', black: 'e7 - d7' },
+            { white: 'd2 - d5', black: '0 - 0 - 0' },
+            { white: '0 - 0 - 0', black: 'g8 - f6' },
+            { white: 'e2(1) - c3', black: 'd7(4) - g4', comment: 'Преждевременно и проигрывает, лучше 8. … h8-d8.' },
+            { white: 'd5(1) - c6', black: 'g4 - g6' },
+            { white: 'c6 - b7', black: 'c8 - d8' },
+            { white: 'g2 - e4', black: 'g6 - g5+' },
+            { white: 'c1 - b1', black: 'g5 - e5' },
+            { white: 'c3 - a4', black: 'h8 - e8' },
+            { white: 'f2 - f3', black: 'e8 - e6' },
+            { white: 'b7 - d5', black: 'd8(1) - e8' },
+            { white: 'h1 - d1', black: 'e5 - h2', 'Позиция черных проиграна.' },
+            { white: 'a4 - c5', black: 'h2 - e2' },
+            { white: 'd5 - d7', black: 'f6 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1 - d7' }
+        ],
+        result: 'Черные сдались.'
+    }
+    'game-1': {
+        whiteGamer: 'Антропов',
+        blackGamer: 'Журавлев',
+        score: '1-0',
+        moves: [
+            { white: 'c1 - d2', black: 'f8 - e9' },
+            { white: 'd1 - d2', black: 'c8 - d7' },
+            { white: 'b1 - c3', black: 'd8 - e7' },
+            { white: 'f1 - g2', black: 'b8 - c6' },
+            { white: 'g1 - e2', black: 'e7 - d7' },
+            { white: 'd2 - d5', black: '0 - 0 - 0' },
+            { white: '0 - 0 - 0', black: 'g8 - f6' },
+            { white: 'e2(1) - c3', black: 'd7(4) - g4', comment: 'Преждевременно и проигрывает, лучше 8. … h8-d8.' },
+            { white: 'd5(1) - c6', black: 'g4 - g6' },
+            { white: 'c6 - b7', black: 'c8 - d8' },
+            { white: 'g2 - e4', black: 'g6 - g5+' },
+            { white: 'c1 - b1', black: 'g5 - e5' },
+            { white: 'c3 - a4', black: 'h8 - e8' },
+            { white: 'f2 - f3', black: 'e8 - e6' },
+            { white: 'b7 - d5', black: 'd8(1) - e8' },
+            { white: 'h1 - d1', black: 'e5 - h2', 'Позиция черных проиграна.' },
+            { white: 'a4 - c5', black: 'h2 - e2' },
+            { white: 'd5 - d7', black: 'f6 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1 - d7' }
+        ],
+        result: 'Черные сдались.'
+    }
+    'game-1': {
+        whiteGamer: 'Антропов',
+        blackGamer: 'Журавлев',
+        score: '1-0',
+        moves: [
+            { white: 'c1 - d2', black: 'f8 - e9' },
+            { white: 'd1 - d2', black: 'c8 - d7' },
+            { white: 'b1 - c3', black: 'd8 - e7' },
+            { white: 'f1 - g2', black: 'b8 - c6' },
+            { white: 'g1 - e2', black: 'e7 - d7' },
+            { white: 'd2 - d5', black: '0 - 0 - 0' },
+            { white: '0 - 0 - 0', black: 'g8 - f6' },
+            { white: 'e2(1) - c3', black: 'd7(4) - g4', comment: 'Преждевременно и проигрывает, лучше 8. … h8-d8.' },
+            { white: 'd5(1) - c6', black: 'g4 - g6' },
+            { white: 'c6 - b7', black: 'c8 - d8' },
+            { white: 'g2 - e4', black: 'g6 - g5+' },
+            { white: 'c1 - b1', black: 'g5 - e5' },
+            { white: 'c3 - a4', black: 'h8 - e8' },
+            { white: 'f2 - f3', black: 'e8 - e6' },
+            { white: 'b7 - d5', black: 'd8(1) - e8' },
+            { white: 'h1 - d1', black: 'e5 - h2', 'Позиция черных проиграна.' },
+            { white: 'a4 - c5', black: 'h2 - e2' },
+            { white: 'd5 - d7', black: 'f6 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1(1) - d7', black: 'd8 - d7' },
+            { white: 'd1 - d7' }
+        ],
+        result: 'Черные сдались.'
+    }
+};
